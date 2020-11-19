5d0b5a0fa522b781e93ae47f2980463b746ac880
Magnus Dauv <mad@alinea.dk>
2020-11-19 20:10:50 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RngHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        
+        private Random rng = new Random();
+
+        private int? random;
+        public int? Random
+        {
+            get { return random; }
+            set
+            {
+                random = value;
+                OnPropertyChanged("Random");
+                OnPropertyChanged("BackgroundColor");                
+            }
+        }
+
+        private Color purple = (Color)ColorConverter.ConvertFromString("#7e00fc");
+        private Color red = (Color)ColorConverter.ConvertFromString("#f70000");
+        private Color orange = (Color)ColorConverter.ConvertFromString("#ff8000");
+        private Color yellow = (Color)ColorConverter.ConvertFromString("#fdfd00");
+
+        public Brush BackgroundColor
+        {
+            get
+            {
+                if (Random > 75)
+                    return new SolidColorBrush(purple);
+                if (Random > 50)
+                    return new SolidColorBrush(red);
+                if (Random > 25)
+                    return new SolidColorBrush(orange);
+                else if (Random > 0)
+                    return new SolidColorBrush(yellow);
+                return new SolidColorBrush();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void randomButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random = rng.Next(1, 101);
+        }
+    }
+}
